f37899367dcd26660cd24a50e24432d5b7ec28d1
vhoreiko <mbox696@gmail.com>
2025-03-24 15:54:28 +0200

    updated angular/material to 15

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from "@angular/material/legacy-dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import {AngularFireStorage} from '@angular/fire/storage';
