7d94bbee2cddda3d7d188e748c3d9ee8bead0eb0
vhoreiko <mbox696@gmail.com>
2025-04-29 22:15:54 +0300

    up npm, tried to setup auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,32 +1,63 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-// import * as firebaseui from 'firebaseui';
-// import {AngularFireAuth} from '@angular/fire/auth';
-// import {Router} from '@angular/router';
-// import firebase from 'firebase/app';
-// import EmailAuthProvider = firebase.auth.EmailAuthProvider;
-// import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
+import { Component, inject, OnDestroy, OnInit, signal } from "@angular/core";
+import { Router } from "@angular/router";
+// import { FirebaseUIModule } from "firebaseui-angular";
 
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: true
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
+  standalone: true,
+  // imports:[FirebaseUIModule],
+  // providers: [
+  //   {
+  //     provide: 'firebaseUIAuthConfig',
+  //     useValue: firebaseUiAuthConfig,
+  //   },
+  // ]
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  // fireAuth = inject(AngularFireAuth);
+  router = inject(Router);
+  // ui: firebaseui.auth.AuthUI;
+  isLoading = signal<boolean>(true);
 
 
-    constructor() {
+  ngOnDestroy(): void {
+    // void this.ui.delete();
+    console.log('destroy');
+  }
 
-    }
+  ngOnInit() {
+    console.log('ngOnInit');
+    // return this.fireAuth.app.then((app) => {
+    //   const uiConfig = {
+    //     signInOptions: [
+    //       firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //     ],
+    //     callbacks: {
+    //       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    //       signInSuccessWithAuthResult: this.onLoginSuccess.bind(this),
+    //       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    //       uiShown: this.uiShown.bind(this),
+    //     },
+    //   };
+    //   // this.ui = new firebaseui.auth.AuthUI(app.auth());
+    //   // this.ui.start("#firebaseui-auth-container", uiConfig);
+    //   // this.ui.disableAutoSignIn();
+    // });
+  }
 
-    ngOnInit() {
+  onLoginSuccess(authResult: { user: never }) {
+    console.log("User logged in:", authResult.user);
+    return this.router.navigateByUrl("/courses");
+  }
 
-    }
-
-    ngOnDestroy() {
-
-    }
+  uiShown() {
+    this.isLoading.set(false);
+    document.getElementById("loader").style.display = "none";
+  }
 }
 
 
