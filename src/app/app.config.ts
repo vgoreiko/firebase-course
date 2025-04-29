b9b432f5f65ad55d223df24cb8bfc09ae138da6d
vhoreiko <mbox696@gmail.com>
2025-04-29 22:26:59 +0300

    formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,53 +2,65 @@ import { provideAnimationsAsync } from "@angular/platform-browser/animations/asy
 import { BrowserModule } from "@angular/platform-browser";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import {connectFirestoreEmulator, getFirestore, provideFirestore} from '@angular/fire/firestore';
-import {connectStorageEmulator, getStorage, provideStorage} from '@angular/fire/storage';
-import {ApplicationConfig} from '@angular/core';
-import { environment} from '../environments/environment';
-import {provideRouter} from '@angular/router';
-import {routes} from './app-routing.module';
-import {connectAuthEmulator, getAuth, provideAuth} from '@angular/fire/auth';
-import {connectFunctionsEmulator, getFunctions, provideFunctions} from '@angular/fire/functions';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from "@angular/fire/firestore";
+import {
+  connectStorageEmulator,
+  getStorage,
+  provideStorage,
+} from "@angular/fire/storage";
+import { ApplicationConfig } from "@angular/core";
+import { environment } from "../environments/environment";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app-routing.module";
+import { connectAuthEmulator, getAuth, provideAuth } from "@angular/fire/auth";
+import {
+  connectFunctionsEmulator,
+  getFunctions,
+  provideFunctions,
+} from "@angular/fire/functions";
 
 console.log(environment.firebase);
-console.log('isProduction:', environment.production);
+console.log("isProduction:", environment.production);
 export const appConfig: ApplicationConfig = {
-    providers: [
-        BrowserModule,
-        provideAnimations(),
-        provideAnimationsAsync(),
-        provideRouter(routes),
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideFirestore(() => {
-            const fireStore = getFirestore();
-            if(environment.useEmulators) {
-                connectFirestoreEmulator(fireStore, 'localhost', 8080);
-                return getFirestore();
-            }
-            return fireStore;
-        }),
-        provideStorage(() => {
-            const storage = getStorage();
-            if (environment.useEmulators) {
-                connectStorageEmulator(storage, 'localhost', 9199);
-                return storage;
-            }
-            return storage;
-        }),
-        provideAuth(() => {
-            const auth = getAuth();
-            if (environment.useEmulators) {
-                connectAuthEmulator(auth, 'http://localhost:9099');
-            }
-            return auth;
-        }),
-        provideFunctions(() => {
-            const functions = getFunctions();
-            if (environment.useEmulators) {
-                connectFunctionsEmulator(functions, 'localhost', 5001);
-            }
-            return functions;
-        }),
-    ],
-}
+  providers: [
+    BrowserModule,
+    provideAnimations(),
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => {
+      const fireStore = getFirestore();
+      if (environment.useEmulators) {
+        connectFirestoreEmulator(fireStore, "localhost", 8080);
+        return getFirestore();
+      }
+      return fireStore;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      if (environment.useEmulators) {
+        connectStorageEmulator(storage, "localhost", 9199);
+        return storage;
+      }
+      return storage;
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.useEmulators) {
+        connectAuthEmulator(auth, "http://localhost:9099");
+      }
+      return auth;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions();
+      if (environment.useEmulators) {
+        connectFunctionsEmulator(functions, "localhost", 5001);
+      }
+      return functions;
+    }),
+  ],
+};
